3d5d019aa9ab0430d133c379a0c538bdb193e0e2
Dheeraj Somashekar <dheeru2605@gmail.com>
2024-11-04 15:06:51 -0600

    voice command added

--- screencapture.py
+++ screencapture.py
@@ -1,1 +1,14 @@
-# Test
+import vosk
+import sounddevice as sd
+
+def recognize_voice_command():
+    model = vosk.Model("path_to_vosk_model")
+    recognizer = vosk.KaldiRecognizer(model, 16000)
+    with sd.InputStream(samplerate=16000, channels=1) as stream:
+        while True:
+            data = stream.read(4000)
+            if recognizer.AcceptWaveform(data):
+                result = recognizer.Result()
+                if "clip" in result:
+                    # Extract time (e.g., 30 seconds) and call clipping function
+                    return process_command(result)
