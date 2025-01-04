95a74d331d26bf7deccc6bf4745cb64100a4080e
Dheeraj Somashekar <dheeru2605@gmail.com>
2025-01-04 00:17:18 -0800

    Added code to app.py

--- UI/app.py
+++ UI/app.py
@@ -5,9 +5,19 @@ def run_app():
     root = tk.Tk()
     root.title("Voice Clips")
 
-    def on_exit():
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            root.destroy()
+    # Default window dimensions
+    window_width = 1200
+    window_height = 700
 
-    root.protocol("WM_DELETE_WINDOW", on_exit)
+    # Get the screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # Calculate the position to center the window
+    position_top = int(screen_height / 2 - window_height / 2)
+    position_right = int(screen_width / 2 - window_width / 2)
+
+    # Set the position and size of the window
+    root.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}')
+
     root.mainloop()
